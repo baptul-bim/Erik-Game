0e1b7054934a86b6ca29f97ad2b662ece5750001
Valerie <victorarsta@gmail.com>
2024-05-01 22:31:27 +0200

    Added a way for contributers to easily Change eriks state with code

--- Assets/Personer/Valerie/V_Scripts/ErikManager.cs
+++ Assets/Personer/Valerie/V_Scripts/ErikManager.cs
@@ -2,23 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Pathfinding;
-
 public class ErikManager : MonoBehaviour
 {
     [SerializeField] private GameObject ErikObj;
     [SerializeField] private AIDestinationSetter ErikDestinationSetter;
     [SerializeField] private AIPath ErikAIPath;
+    [SerializeField] private string ErikCurrentState;
+
+    private string[] avalibleStates = { "Chase", "Lurk", "Patrol", "Idle" };
+
+    //private List<Action> AvalibleStates;
+    
+
     // Start is called before the first frame update
     void Start()
     {
         ErikDestinationSetter = ErikObj.GetComponent<AIDestinationSetter>();
         ErikAIPath = ErikObj.GetComponent<AIPath>();
+
+        SetErikState("Patrol");
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (int.TryParse(Input.inputString, out int numberPressed)) //Debug tool: Changes Erik's speed to the corresponding numeric key pressed.
+        {
+            SetErikSpeed(numberPressed);
+        }
+    }
+
+    /// <summary>
+    /// Changes the current state of the Erik AI.
+    /// <list type="States">
+    /// Avalible States:
+    /// <item>Chase, Idle, Patrol, Lurk</item>
+    /// </list>
+    /// </summary>
+    /// <param name="State"></param>
+    public void SetErikState(string stateName)
+    {
+     
+        foreach (string state in avalibleStates)
+        {
+            if (state == stateName)
+            {
+                Invoke(stateName, 0.0f);
+                ErikCurrentState = stateName;
+            }
+        }
         
+
     }
 
     public void SetErikTarget(GameObject TargetObj)
@@ -26,10 +61,36 @@ public class ErikManager : MonoBehaviour
         ErikDestinationSetter.target = TargetObj.transform;
     }
 
-    public void SetErikMaxSpeed(float MaxSpeed)
+    
+
+
+    public void SetErikSpeed(float MaxSpeed = 1, bool CanMove = true) 
     {
+        //If method is called without specifying parameters, each will result in its default value.
+
         ErikAIPath.maxSpeed = MaxSpeed;
+        ErikAIPath.canMove = CanMove;
     }
 
+    private void Chase()
+    {
+        SetErikSpeed(3);
+    }
+
+    private void Idle()
+    {
+        SetErikSpeed(0);
+    }
+
+    private void Patrol()
+    {
+        SetErikSpeed(1.5f);
+    }
+
+    private void Lurk()
+    {
+        SetErikSpeed(0.5f);
+    }
 
 }
+
---
+++ Assets/Personer/Valerie/V_Scripts/TargetPoint.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetPoint : MonoBehaviour
+{
+    [SerializeField] GameObject TargetToMove;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            RaycastAndTeleport();
+            ErikManager amogus = FindObjectOfType<ErikManager>();
+            amogus.SetErikState("Chase");
+
+        }
+    }
+
+    private void RaycastAndTeleport()
+    {
+
+        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+        if (Physics.Raycast(mouseRay, out hitInfo))
+        {
+            print(hitInfo.collider.name);
+            TargetToMove.transform.position = hitInfo.point;
+        }
+
+    }
+    
+}
