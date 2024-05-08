002baac6c6a4850855a74f79307dbd29a82c13fd
YodaOnKetamin <Simon.Ulander.06@gmail.com>
2024-05-08 09:45:01 +0200

    audio slider and toggle

--- Assets/UIoMeny/Simon/Scripts Simon/UnderlineText.cs
+++ Assets/UIoMeny/Simon/Scripts Simon/UnderlineText.cs
@@ -25,7 +25,7 @@ public class UnderlineText : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentText.text = Text;
     }
 
     // Update is called once per frame
