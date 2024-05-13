1df5ac03978a236a8478aace2fc48f135f9797b9
Line <line.thomsson@elev.ga.lbs.se>
2024-05-13 15:53:17 +0200

    klar med health UI
    den startar på full och sen ändrar sprite

--- Assets/UIoMeny/Milo/script/Health.cs
+++ Assets/UIoMeny/Milo/script/Health.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Sprite twoHealth;
+    public Sprite oneHealth;
+    int tempHealth;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (tempHealth == 2)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = twoHealth;
+        }
+
+        if (tempHealth == 1)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = oneHealth;
+        }
     }
 }
