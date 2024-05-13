ee6dce9138e7d513c9066cc7b244ad8e5430116b
Line <line.thomsson@elev.ga.lbs.se>
2024-05-13 16:03:43 +0200

    la till kommentarer

--- Assets/UIoMeny/Milo/script/Health.cs
+++ Assets/UIoMeny/Milo/script/Health.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+
+    // det börjar på full health så det behövs bara 2 i script
     public Sprite twoHealth;
     public Sprite oneHealth;
+    
+    // byt ut detta med vad den faktiska variabeln för att ändra health är
     int tempHealth;
 
     // Update is called once per frame
     void Update()
     {
+        // om health är x/3 så byter det sprite till vi skrev där uppe. 
         if (tempHealth == 2)
         {
             this.gameObject.GetComponent<SpriteRenderer>().sprite = twoHealth;
