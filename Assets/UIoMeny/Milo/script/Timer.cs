290a64e3acb9cfbb0005faf11e081aed4df18db7
Line <line.thomsson@elev.ga.lbs.se>
2024-05-14 13:32:49 +0200

    trying to make timer

--- Assets/UIoMeny/Milo/script/Timer.cs
+++ Assets/UIoMeny/Milo/script/Timer.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Timer : MonoBehaviour
 {
+    public float timeLeft;
+    public Text timer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        timeLeft = 60;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timeLeft -= Time.deltaTime;
     }
 }
