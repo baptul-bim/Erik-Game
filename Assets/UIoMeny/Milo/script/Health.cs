c897fccc67e38c38aa996a5a4493f3af88269cc3
Line <line.thomsson@elev.ga.lbs.se>
2024-05-20 13:40:14 +0200

    trying to make health work

--- Assets/UIoMeny/Milo/script/Health.cs
+++ Assets/UIoMeny/Milo/script/Health.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
@@ -8,17 +10,23 @@ public class Health : MonoBehaviour
     // det börjar på full health så det behövs bara 2 i script
     public Sprite twoHealth;
     public Sprite oneHealth;
-    
+    public Image image;
+
     // byt ut detta med vad den faktiska variabeln för att ändra health är
     int tempHealth;
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            tempHealth--;
+        }
+
         // om health är x/3 så byter det sprite till vi skrev där uppe. 
         if (tempHealth == 2)
         {
-            this.gameObject.GetComponent<SpriteRenderer>().sprite = twoHealth;
+            image.GetComponent<SpriteRenderer>().sprite = twoHealth;
         }
 
         if (tempHealth == 1)
