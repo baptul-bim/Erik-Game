77dac818c0b3f410b6705b46669dafa5695c2287
CaffeineAdictedAlpaca <119925733+CaffeineAdictedAlpaca@users.noreply.github.com>
2024-05-27 13:51:27 +0200

    Stunned

--- Assets/Scripts/Spelare/PlayerHealthManager.cs
+++ Assets/Scripts/Spelare/PlayerHealthManager.cs
@@ -4,20 +4,34 @@ using UnityEngine;
 
 public class PlayerHealthManager : MonoBehaviour
 {
-    public maxHearts = 3;
-    public heartsLeft = 3;
+    public int maxHearts = 3;
+    public int heartsLeft = 3;
+    float timer;
+    public bool stunned;
 
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
+    private int prevHearts = 3;
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        timer += Time.deltaTime;
+        if (prevHearts > heartsLeft)
+        {
+            stunned = true;
+            prevHearts = heartsLeft;
+        }
 
-    
+        if (stunned)
+        {
+            timer = 0;
+            if(timer >= 2)
+            {
+                stunned = false;
+            }
+        }
+    }
 }
