9ceb4fa60c0a120533f4b0a283886fe7ffa22622
baptul-bim <baptul.bim@gmail.com>
2024-05-28 13:50:16 +0200

    changes 2

--- Assets/Scripts/Spelare/PlayerHealthManager.cs
+++ Assets/Scripts/Spelare/PlayerHealthManager.cs
@@ -4,19 +4,32 @@ using UnityEngine;
 
 public class PlayerHealthManager : MonoBehaviour
 {
-    public maxHearts = 3;
-    public heartsLeft = 3;
+    public int maxHearts = 3;
+    public int heartsLeft = 0;
+
+    GameObject _Erikmanager;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _Erikmanager = GameObject.FindGameObjectWithTag("ErikManager");
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+    public void TakeDamage()
+    {
+        heartsLeft--;
+        if (heartsLeft <= 0)
+        {
+            
+            _Erikmanager.GetComponent<ErikAnimationManager>().PlayJumpscare();
+        }
     }
 
     
