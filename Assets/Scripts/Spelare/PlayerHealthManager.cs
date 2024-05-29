df85059c519ff264aa8c2d7512a3dfeed89cff51
Valerie <victorarsta@gmail.com>
2024-05-29 13:56:14 +0200

    The Wizard helped me :,)

--- Assets/Scripts/Spelare/PlayerHealthManager.cs
+++ Assets/Scripts/Spelare/PlayerHealthManager.cs
@@ -5,22 +5,14 @@ using UnityEngine;
 public class PlayerHealthManager : MonoBehaviour
 {
     public int maxHearts = 3;
-<<<<<<< HEAD
     public int heartsLeft = 0;
 
     GameObject _Erikmanager;
-=======
-    public int heartsLeft = 3;
->>>>>>> parent of 68a9d72 (Revert "Merge remote-tracking branch 'upstream/main' into ErikGrupp")
 
     // Start is called before the first frame update
     void Start()
     {
-<<<<<<< HEAD
         _Erikmanager = GameObject.FindGameObjectWithTag("ErikManager");
-=======
-        
->>>>>>> parent of 68a9d72 (Revert "Merge remote-tracking branch 'upstream/main' into ErikGrupp")
     }
 
     // Update is called once per frame
@@ -29,7 +21,6 @@ public class PlayerHealthManager : MonoBehaviour
         
     }
 
-<<<<<<< HEAD
 
     public void TakeDamage()
     {
@@ -42,7 +33,5 @@ public class PlayerHealthManager : MonoBehaviour
         }
     }
 
-=======
->>>>>>> parent of 68a9d72 (Revert "Merge remote-tracking branch 'upstream/main' into ErikGrupp")
     
 }
--- Assets/tasks/Albin/Scripts/rapport.cs
+++ Assets/tasks/Albin/Scripts/rapport.cs
@@ -1,3 +1,4 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,11 +8,13 @@ public class rapport : MonoBehaviour
     [SerializeField]
     GameObject rapportUI;
     [SerializeField]
-    GameObject färdig_rapportUI;
+    GameObject fardig_rapportUI;
     [SerializeField]
     GameObject facitUI;
-    //[SerializeField]
-    //GameObject text;
+    [SerializeField]
+    GameObject text;
+    [SerializeField]
+    GameObject tip;
     [SerializeField]
     GameObject row1;
     [SerializeField]
@@ -137,115 +140,120 @@ public class rapport : MonoBehaviour
         row35.SetActive(false);
         row36.SetActive(false);
         row37.SetActive(false);
-        f�rdig_rapportUI.SetActive(false);
+        fardig_rapportUI.SetActive(false);
         rapportUI.SetActive(false);
         facitUI.SetActive(false);
         text.SetActive(false);
         tip.SetActive(false);
     }
 
+    void start_rapport_task()
+    {
+        if (facit == false)
+        {
+            rapportUI.SetActive(true);
+            text.SetActive(true);
+            tip.SetActive(true);
+        }
+        if (facit == true)
+        {
+            tasking = true;
+            row1.SetActive(true);
+            row2.SetActive(true);
+            row3.SetActive(true);
+            row4.SetActive(true);
+            row5.SetActive(true);
+            row6.SetActive(true);
+            row7.SetActive(true);
+            row8.SetActive(true);
+            row9.SetActive(true);
+            row10.SetActive(true);
+            row11.SetActive(true);
+            row12.SetActive(true);
+            row13.SetActive(true);
+            row14.SetActive(true);
+            row15.SetActive(true);
+            row16.SetActive(true);
+            row17.SetActive(true);
+            row18.SetActive(true);
+            row19.SetActive(true);
+            row20.SetActive(true);
+            row21.SetActive(true);
+            row22.SetActive(true);
+            row23.SetActive(true);
+            row24.SetActive(true);
+            row25.SetActive(true);
+            row26.SetActive(true);
+            row27.SetActive(true);
+            row28.SetActive(true);
+            row29.SetActive(true);
+            row30.SetActive(true);
+            row31.SetActive(true);
+            row32.SetActive(true);
+            row33.SetActive(true);
+            row34.SetActive(true);
+            row35.SetActive(true);
+            row36.SetActive(true);
+            row37.SetActive(true);
+            fardig_rapportUI.SetActive(true);
+            facitUI.SetActive(true);
+        }
+    }
+    void end_rapport_task()
+    {
+        typing = 0;
+        tasking = false;
+        row1.SetActive(false);
+        row2.SetActive(false);
+        row3.SetActive(false);
+        row4.SetActive(false);
+        row5.SetActive(false);
+        row6.SetActive(false);
+        row7.SetActive(false);
+        row8.SetActive(false);
+        row9.SetActive(false);
+        row10.SetActive(false);
+        row11.SetActive(false);
+        row12.SetActive(false);
+        row13.SetActive(false);
+        row14.SetActive(false);
+        row15.SetActive(false);
+        row16.SetActive(false);
+        row17.SetActive(false);
+        row18.SetActive(false);
+        row19.SetActive(false);
+        row20.SetActive(false);
+        row21.SetActive(false);
+        row22.SetActive(false);
+        row23.SetActive(false);
+        row24.SetActive(false);
+        row25.SetActive(false);
+        row26.SetActive(false);
+        row27.SetActive(false);
+        row28.SetActive(false);
+        row29.SetActive(false);
+        row30.SetActive(false);
+        row31.SetActive(false);
+        row32.SetActive(false);
+        row33.SetActive(false);
+        row34.SetActive(false);
+        row35.SetActive(false);
+        row36.SetActive(false);
+        row37.SetActive(false);
+        fardig_rapportUI.SetActive(false);
+        rapportUI.SetActive(false);
+        facitUI.SetActive(false);
+        text.SetActive(false);
+        tip.SetActive(false);
+    }
+
+    void cleared_rapport_task()
+    {
+        SendMessageUpwards("rapport_task_cleared",SendMessageOptions.RequireReceiver);
+    }
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            if (facit == false)
-            {
-                rapportUI.SetActive(true);
-                text.SetActive(true);
-                tip.SetActive(true);
-            }
-            if (facit == true) 
-            {
-                tasking = true;
-                row1.SetActive(true);
-                row2.SetActive(true);
-                row3.SetActive(true);
-                row4.SetActive(true);
-                row5.SetActive(true);
-                row6.SetActive(true);
-                row7.SetActive(true);
-                row8.SetActive(true);
-                row9.SetActive(true);
-                row10.SetActive(true);
-                row11.SetActive(true);
-                row12.SetActive(true);
-                row13.SetActive(true);
-                row14.SetActive(true);
-                row15.SetActive(true);
-                row16.SetActive(true);
-                row17.SetActive(true);
-                row18.SetActive(true);
-                row19.SetActive(true);
-                row20.SetActive(true);
-                row21.SetActive(true);
-                row22.SetActive(true);
-                row23.SetActive(true);
-                row24.SetActive(true);
-                row25.SetActive(true);
-                row26.SetActive(true);
-                row27.SetActive(true);
-                row28.SetActive(true);
-                row29.SetActive(true);
-                row30.SetActive(true);
-                row31.SetActive(true);
-                row32.SetActive(true);
-                row33.SetActive(true);
-                row34.SetActive(true);
-                row35.SetActive(true);
-                row36.SetActive(true);
-                row37.SetActive(true);
-                f�rdig_rapportUI.SetActive(true);
-                facitUI.SetActive(true);
-            }
-        }
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            typing = 0;
-            tasking = false;
-            row1.SetActive(false);
-            row2.SetActive(false);
-            row3.SetActive(false);
-            row4.SetActive(false);
-            row5.SetActive(false);
-            row6.SetActive(false);
-            row7.SetActive(false);
-            row8.SetActive(false);
-            row9.SetActive(false);
-            row10.SetActive(false);
-            row11.SetActive(false);
-            row12.SetActive(false);
-            row13.SetActive(false);
-            row14.SetActive(false);
-            row15.SetActive(false);
-            row16.SetActive(false);
-            row17.SetActive(false);
-            row18.SetActive(false);
-            row19.SetActive(false);
-            row20.SetActive(false);
-            row21.SetActive(false);
-            row22.SetActive(false);
-            row23.SetActive(false);
-            row24.SetActive(false);
-            row25.SetActive(false);
-            row26.SetActive(false);
-            row27.SetActive(false);
-            row28.SetActive(false);
-            row29.SetActive(false);
-            row30.SetActive(false);
-            row31.SetActive(false);
-            row32.SetActive(false);
-            row33.SetActive(false);
-            row34.SetActive(false);
-            row35.SetActive(false);
-            row36.SetActive(false);
-            row37.SetActive(false);
-            f�rdig_rapportUI.SetActive(false);
-            rapportUI.SetActive(false);
-            facitUI.SetActive(false);
-            text.SetActive(false);
-            tip.SetActive(false);
-        }
         if (tasking == true)
         {
             if (Input.anyKeyDown)
@@ -399,11 +407,11 @@ public class rapport : MonoBehaviour
             if (typing >= 38)
             {
                 row37.SetActive(false);
-                done = true;
-                f�rdig_rapportUI.SetActive(false);
+                cleared_rapport_task();
+                fardig_rapportUI.SetActive(false);
                 rapportUI.SetActive(false);
                 facitUI.SetActive(false);
-                //text.SetActive(false);
+                text.SetActive(false);
             }
         }
     }
